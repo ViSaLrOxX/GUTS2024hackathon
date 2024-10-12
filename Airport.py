d2de4a2ac9901271dd5627e781eb534adcf4033c
s.nikolov1000@gmail.com <s.nikolov1000@gmail.com>
2024-10-12 14:49:34 +0100

    fix circular import

--- Airport.py
+++ Airport.py
@@ -1,4 +1,4 @@
-import Plane
+
 class Airport:
     def __init__(self, 
                  xCoord: float, 
@@ -6,13 +6,17 @@ class Airport:
                  arrivals: list, 
                  departures: list, 
                  max_capacity: int,
-                 num_planes: int):
+                 num_planes: int,
+                 airport_connections: dict):
         self.x = xCoord
         self.y = yCoord
         self.arrivals = arrivals
         self.departures = departures
         self.max_capacity = max_capacity
         self.num_planes = num_planes
+
+    def add_connection():
+        pass
     def update(self, time):
         pass
 
--- Game.py
+++ Game.py
@@ -1,22 +1,29 @@
 from datetime import datetime
 from Time import Time
 import csv
-
+from Airport import Airport
 class Game:
 
     def __init__(self):
         # f = open("data.csv", "r")
         # self.airports = list(csv.reader(f, delimiter=","))
         # f.close()
-
+        self.airport_connections = {}
         self.time = Time(int(datetime.now().strftime('%H:%M:%S')[:2]), int(datetime.now().strftime('%H:%M:%S')[3:5]))
         print(self.time)
         self.bottom = [49.605015, -12.482707] # Bottom left coordinates
         self.top = [61.160198, 1.686227] # Top right coordinates
 
     def update(self):
-        self.time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.time.set_time(2, 1)
 
+    def add_connection(self, main: Airport, other:Airport, weight: float):
+        self.airport_connections[main][other] = weight
+        pass
 if __name__ == "__main__":
-    Game()
-    print(2)
+    game1 = Game()
+    game2 = game1
+    print(game2)
+    game2.update()
+    print(game2)
+    print(game1 == game2)
